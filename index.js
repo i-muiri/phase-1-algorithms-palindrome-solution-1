1c983198f5e7a725570ad99744e1cba19754c9db
i-muiri <ianmuiri99@gmail.com>
2022-12-16 12:25:55 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const reverseWord =word.split('').reverse().join('')
+  if(word === reverseWord)
+  {
+    return true;
+  }
+  else
+      return false;
 }
-
 /* 
   Add your pseudocode here
-*/
+*///split word to array
+//need to reverse the word
+//an else if condition is necessary
+//return true or false if they match
 
 /*
   Add written explanation of your solution here
 */
+//in the function palindrome there is a variable reverseWord
+//used the argument in the function and split it into an array
+//i reversed the new array
+//joined it into a string
+//compared the argument and the reverse
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
+
   console.log("=>", isPalindrome("racecar"));
 
   console.log("");
